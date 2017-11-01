d6e41ef3501a7b23e191f774d77fec67fba12a38
Marie-Charlotte Eyb <marie-charlotte@gmx.de>
2017-11-01 16:47:19 +0000

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-5.times do
-	restaurant = Restaurant.new(
+require 'faker'
+10.times do
+	Restaurant.create(
 		name: Faker::Name.last_name,
-		address: Faker::Address.street_address + " in " + Faker::Address.city,
+		address: Faker::Address.street_address, 
+		category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+		phone_number: Faker::Number.number
 		)
-	restaurant.save
 end
